ee982c6b25039480b8cdcf1ee2d2565c6d902995
jaime tames <tamesjaime96@gmail.com>
2023-11-28 17:03:03 -0600

    commit

--- assets/script.js
+++ assets/script.js
@@ -4,18 +4,13 @@ console.log(topics);
 var topic = "HTML";
 
 if ( topic === 'HTML'){
-
     console.log ("Let's study HTML!");
 } else if (topic === 'CSS'){
-
     console.log ("Let's study CSS!")
 } else if (topic === Git){
-
     console.log("Let's study Git")
 } else if (topic === JavaScript) {
-
  console.log("let study JavaScript")
-
 } else {   
 console.log('Please try gaine !');
 }
