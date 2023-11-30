4f149cadb18f216d02267ee0f0e10898014f43b1
jaime tames <tamesjaime96@gmail.com>
2023-11-30 10:35:28 -0600

    commit

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var topics = ['HTML','CSS','Git','JavaScript'];
 
 function listTopics(){
-    for ( var x = 0; x < topics.length; x++ ) {
-        console.log(topix[x]);
+    for ( var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
     }
 }
 function selectTopic() {
