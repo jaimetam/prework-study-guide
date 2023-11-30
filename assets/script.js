186ff2e63e7eea73335839b01819fb4e2c008d6f
jaime tames <tamesjaime96@gmail.com>
2023-11-30 10:10:51 -0600

    commit

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,26 @@
 var topics = ['HTML','CSS','Git','JavaScript'];
 
+function listTopics(){
+    for ( var x = 0; x < topics.length; x++ ) {
+        console.log(topix[x]);
+    }
+}
+
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);   
 }
 
-
-/*
-if ( topic === 'HTML'){
-    console.log ("Let's study HTML!");
-} else if (topic === 'CSS'){
-    console.log ("Let's study CSS!")
-} else if (topic === Git){
-    console.log("Let's study Git")
-} else if (topic === JavaScript) {
- console.log("let study JavaScript")
-} else {   
-console.log('Please try gaine !');
+function selectTopic(){
+    if ( topic === 'HTML'){
+        console.log ("Let's study HTML!");
+    } else if (topic === 'CSS'){
+        console.log ("Let's study CSS!")
+    } else if (topic === Git){
+        console.log("Let's study Git")
+    } else if (topic === JavaScript) {
+     console.log("let study JavaScript")
+    } else {   
+    console.log('Please try gaine !');
+    }
+    
 }
-*/
