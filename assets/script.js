972d7efd2312eb631bbe8b90502e90bcf32ea51f
jaime tames <tamesjaime96@gmail.com>
2023-11-30 10:51:28 -0600

    commit

--- assets/script.js
+++ assets/script.js
@@ -20,5 +20,7 @@ function selectTopic() {
     console.log('Please try gaine !');
     }
 }
+console.log('Here are the topics we learned through Prework:');
 listTopics();
+console.log('Which topic should we study first?');
 selectTopic();
